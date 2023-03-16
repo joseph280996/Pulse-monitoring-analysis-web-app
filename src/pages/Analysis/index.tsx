4896796c416e87d38cfe9a59080e8a8313444e7d
Tung Pham <joseph.pham280996@gmail.com>
2023-03-16 00:32:30 -0400

    Create get min zoom value function

--- src/pages/Analysis/index.tsx
+++ src/pages/Analysis/index.tsx
@@ -1,5 +1,15 @@
 import AnalysisView from "./Analysis.view";
 
+const getMinXValueFromDataList = (dataList: any[]) => {
+    let minValue = 0;
+    dataList.forEach(data => {
+        const firstElementTimeStamp = data[0].timeStamp;
+        if(firstElementTimeStamp < minValue){
+            minValue = 0;
+        }
+    })
+}
+
 const Analysis = () => {
     const data: any[] = [];
 
