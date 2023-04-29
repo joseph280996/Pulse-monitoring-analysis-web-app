1b5e08d6a321e5c0e06552ad279bc2feb6091f75
Tung Pham <joseph.pham280996@gmail.com>
2023-04-29 17:39:43 -0400

    remove unuse files

--- src/apis/interfaces/IDiagnosisService.ts
+++
@@ -1,7 +0,0 @@
-import type Diagnosis from "../../dataObjects/Diagnosis";
-import Record from "../../dataObjects/Record";
-
-export interface IDiagnosisService {
-  getRecordsByDiagnosisId: (id: number) => Promise<{ records?: Record[] }>;
-  getAllDiagnoses: () => Promise<{ diagnoses?: Diagnosis[] }>;
-}
